f32941200e8ef91c1b250a0f2fced4f56062a81e
SA-LU <shl423@eng.ucsd.edu>
2016-01-21 09:56:42 -0800

    Rename the babies23.smoke_wt to s_wt, babies23.nosmoke_wt to ns_wt

--- hw1/init.R
+++ hw1/init.R
@@ -9,25 +9,27 @@ head(babies23)
 # See distribution of smokers
 # 692 samples
 babies23.smoke = babies23[which(babies23$smoke != 0), ]
+s.wt = babies23.smoke&smoke
 #ggplot(data, aes(babies23.smoke$wt)) + geom_histogram() 
-hist(babies23.smoke$wt)
-qqnorm(babies23.smoke$wt)
+hist(s.wt)
+qqnorm(s.wt)
 
 
 # See distribution of non smokers
 #544 samples
 babies23.nosmoke = babies23[which(babies23$smoke == 0), ]
-hist(babies23.nosmoke$wt)
-qqnorm(babies23.nosmoke$wt)
+ns.wt = babies23.nosmoke$wt
+hist(ns.wt)
+qqnorm(ns.wt)
 
 
 # See overlapped distribution
-hist(babies23.smoke$wt, col=rgb(0.1,0.1,0.5,0.5))
-hist(babies23.nosmoke$wt, col=rgb(0.7,0.5,0.1,0.5), add=T)
+hist(s.wt, col=rgb(0.1,0.1,0.5,0.5))
+hist(ns.wt, col=rgb(0.7,0.5,0.1,0.5), add=T)
 
 # See overlapped distribution with boxplots
 #boxplot(babies23.smoke$wt, col=rgb(0.1,0.1,0.5,0.5), horizontal=T)
 #boxplot(babies23.nosmoke$wt, col=rgb(0.7,0.5,0.1,0.5), add=T, horizontal=T)
 
-boxplot(babies23.smoke$wt, babies23.nosmoke$wt)
+boxplot(s.wt, ns.wt)
 
