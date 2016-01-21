bde7449679452df38a6142c3bff211897c932ec1
SA-LU <shl423@eng.ucsd.edu>
2016-01-21 09:49:08 -0800

    Added qqnorms to both non-smoke and smoke

--- hw1/init.R
+++ hw1/init.R
@@ -1,24 +1,33 @@
 # Read in data
+library(moments)
+library(ggplot2)
 babies23 = read.table("babies23.txt", header = TRUE)
 babies_dot = read.table("babies..txt", header = TRUE)
 
 head(babies23)
 
 # See distribution of smokers
+# 692 samples
 babies23.smoke = babies23[which(babies23$smoke != 0), ]
 #ggplot(data, aes(babies23.smoke$wt)) + geom_histogram() 
 hist(babies23.smoke$wt)
+qqnorm(babies23.smoke$wt)
+
 
 # See distribution of non smokers
+#544 samples
 babies23.nosmoke = babies23[which(babies23$smoke == 0), ]
 hist(babies23.nosmoke$wt)
+qqnorm(babies23.nosmoke$wt)
+
 
 # See overlapped distribution
 hist(babies23.smoke$wt, col=rgb(0.1,0.1,0.5,0.5))
 hist(babies23.nosmoke$wt, col=rgb(0.7,0.5,0.1,0.5), add=T)
 
 # See overlapped distribution with boxplots
-boxplot(babies23.smoke$wt, col=rgb(0.1,0.1,0.5,0.5), horizontal=T)
-boxplot(babies23.nosmoke$wt, col=rgb(0.7,0.5,0.1,0.5), add=T, horizontal=T)
+#boxplot(babies23.smoke$wt, col=rgb(0.1,0.1,0.5,0.5), horizontal=T)
+#boxplot(babies23.nosmoke$wt, col=rgb(0.7,0.5,0.1,0.5), add=T, horizontal=T)
 
 boxplot(babies23.smoke$wt, babies23.nosmoke$wt)
+
