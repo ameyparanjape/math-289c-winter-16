63e5f47cd19787b97b34733c2622b4b256989d4f
massoudTheDude <mahermassoud@gmail.com>
2016-02-01 19:46:38 -0800

    Did scenario1

---
+++ hw2/scenario1.R
@@ -0,0 +1,9 @@
+# Proportion of student who played video games the week before the survey
+prop.played = length( which(time.played != 0) ) / sample.size
+
+# Estimation for standard error, found on slide 36
+prop.played.se = sqrt( prop.played*(1 - prop.played)/(sample.size - 1) )  * sqrt((pop.size - sample.size) / pop.size)
+
+# Confidence interval for proportion found on slide 39
+error = 1.96*prop.played.se/sqrt.n
+prop.cint = c(prop.played - error,prop.played + error)
--- hw2/variables.R
+++ hw2/variables.R
@@ -1,5 +1,16 @@
 # Data is all data
 data = read.table("videodata.txt", header=TRUE)
 
+# Sample size = 91
+sample.size = nrow(data)
+
+# Sqrt of sample size 91
+sqrt.n = sqrt(sample.size)
+
+# Population size = 314
+pop.size = 314
+
 # How many hours played week before survey
-hours = data$time
+time.played = data$time
+
+
