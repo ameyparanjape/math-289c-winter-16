09eecc2f4eee76963bffd7bde68e905b0f07d1cf
recorrea <recorrea@ucsd.edu>
2016-02-08 22:59:07 -0800

    Added my variables

--- hw2/variables.R
+++ hw2/variables.R
@@ -19,8 +19,21 @@ played.arcade = data[ which(data$time != 0 & data$where == 1), ]
 # Students who have access to a PC with a CDROM and gamed the week before
 played.pc = data[ which( data$time != 0 & (data$own == 1 | data$home == 1) & data$cdrom == 1), ]
 
-# Students who said they play semesterly
+# Students who said they play semsterly
 semesterly = data[which(data$freq == 4),]
 
 # Students who said they play monthly
 monthly = data[which(data$freq == 3),]
+
+# Students who said they play weekly
+weekly = data[which(data$freq == 2),]
+
+# Students who said they play daily
+daily = data[which(data$freq == 1),]
+
+# Students who answered a valid answer for freq
+valid = data[which(data$freq != 99),]
+valid = valid[which(valid$busy != 99),]
+
+# Create the boxplot without outliers
+labels <- c("Daily", "Weekly", "Monthly", "Semesterly")
