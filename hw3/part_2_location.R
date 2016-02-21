868239a90e8e947e499a9ed67bc6e6639031d1e9
massoudTheDude <mahermassoud@gmail.com>
2016-02-21 14:07:38 -0800

    Added distance code

---
+++ hw3/part_2_location.R
@@ -0,0 +1,49 @@
+# Read in distances and do a line plot
+locs = read.table(file = "hcmv.txt", header = T)
+dists = (locs[2:(nrow(locs)),1] - locs[1:(nrow(locs)-1),1])
+
+# Generate random locations, get distances, and do a line plot
+rand_locs = sort(sample(1:229354, 296))
+rand_dists = (rand_locs[2:(length(rand_locs))] - rand_locs[1:(length(rand_locs)-1)])
+
+dists = data.frame(dists)
+rand_dists = data.frame(rand_dists)
+
+# Plot histograms
+library(ggplot2)
+gg_dists <- ggplot(dists, aes(x=dists))
+gg_rand_dists <- ggplot(rand_dists, aes(rand_dists)) 
+
+
+gg_dists + geom_histogram(binwidth = 500) + ggtitle('Distance between consecutive palindromes')
+gg_rand_dists + geom_histogram(binwidth = 500) + ggtitle('Distance between consecutive palindromes of randomly generated data')
+
+# Plot box plots
+boxplot(dists$dists, rand_dists$rand_dists, names=c('Actual', 'Randomly generated'),main="Distance between consecutive palindromes")
+
+#############################
+# Repeat for triples
+# Read in distances and do a line plot
+dists_three = (locs[3:(nrow(locs)),1] - locs[1:(nrow(locs)-2),1])
+
+# Generate random locations, get distances, and do a line plot
+rand_dists_three = (rand_locs[3:(length(rand_locs))] - rand_locs[1:(length(rand_locs)-2)])
+
+dists_three = data.frame(dists_three)
+rand_dists_three = data.frame(rand_dists_three)
+
+# Plot histograms
+library(ggplot2)
+gg_dists_three <- ggplot(dists_three, aes(x=dists_three))
+gg_rand_dists_three <- ggplot(rand_dists_three, aes(rand_dists_three))
+
+gg_dists_three + geom_histogram(binwidth = 500) + ggtitle('Distance between 3 consecutive palindromes')
+gg_rand_dists_three + geom_histogram(binwidth = 500) + ggtitle('Distance between 3 consecutive palindromes of randomly generated data')
+
+# Plot box plots
+boxplot(dists_three$dists_three, rand_dists_three$rand_dists_three, names=c('Actual', 'Randomly generated'),main="Distance between 3 consecutive palindromes")
+
+
+
+
+
