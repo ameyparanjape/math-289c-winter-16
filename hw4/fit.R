6da5df8ada23c79233bfe6fefb760c5d93896dd2
massoudTheDude <mahermassoud@gmail.com>
2016-03-01 20:53:46 -0800

    residual plot

--- hw4/fit.R
+++ hw4/fit.R
@@ -17,4 +17,17 @@ plot(gauge.avg[,1], gauge.avg[,'gain'])
 # Plot data with logged gains averaged into groups of 10
 plot(gauge.avg[,1], gauge.avg[,'ln.gain'])
 
-fit = lm(density ~ ln(gain), data = gauge)
+fit = lm(gauge.avg[,'ln.gain'] ~ gauge.avg[,1], data = gauge.avg)
+abline(fit$coefficients[1],fit$coefficients[2])
+#plot(fit)
+
+residuals = fit$residuals
+
+# Demonstrate constaant variability
+plot(gauge.avg[,'ln.gain'], residuals)
+
+# Plots to do
+# x vs. y
+# ln.gain vs. residuals
+# qqplot of residuals
+# data with logged gains averaged into groups of 10
