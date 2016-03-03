805ff4054c0b7aa2729c8a16d531f79a4b33b07d
massoudTheDude <mahermassoud@gmail.com>
2016-03-02 18:59:09 -0800

    Added ggplot

--- hw4/fit.R
+++ hw4/fit.R
@@ -5,29 +5,59 @@ gauge['ln.gain'] = log(gauge['gain'])
 # Average data into groups of 10
 gauge.avg = aggregate( gauge[,2:3], list(gauge$density), mean)
 
+library(ggplot2)
+
 # Plot raw data
 plot(gauge[,'density'], gauge[,'gain'])
+raw.plot =  ggplot(gauge, aes(gauge[,'density'], gauge[,'gain'])) +
+            geom_point(shape=1, colour="blue") +
+            xlab("Gain") +
+            ylab("Density")+
+            ggtitle("Raw Gain vs. Density")
+raw.plot
 
 # Plot logged data
 plot(gauge[,'density'], gauge[,'ln.gain'])
+#ln.plot =   ggplot(gauge, aes(gauge[,'density'], gauge[,'ln.gain'])) +
+#            geom_point(shape=1) +
+#           geom_smooth(method='lm') +
+#            xlab("ln(Gain)") +
+#            ylab("Density")+
+#            ggtitle("ln(Gain) vs. Density")
+#ln.plot
 
 # Plot data averaged into groups of 10
 plot(gauge.avg[,1], gauge.avg[,'gain'])
 
-# Plot data with logged gains averaged into groups of 10
-plot(gauge.avg[,1], gauge.avg[,'ln.gain'])
-
 fit = lm(gauge.avg[,'ln.gain'] ~ gauge.avg[,1], data = gauge.avg)
+# Plot data with logged gains averaged into groups of 10
+plot(gauge.avg[,1], gauge.avg[,'ln.gain'], main = "Density vs. ln(gain) with averaged data",
+     xlab = "Density", ylab = "ln(gain)")
 abline(fit$coefficients[1],fit$coefficients[2])
+ln.avg.plot =   ggplot(gauge.avg, aes(gauge.avg[,1], gauge.avg[,'ln.gain'])) +
+                geom_point(shape=1, colour="blue") +
+                ylab("ln(Gain)") +
+                xlab("Density")+
+                ggtitle("Density vs. ln(gain) with averaged data") +
+                geom_abline(slope=fit$coefficients[[2]],intercept=fit$coefficients[[1]],colour = "red")
+ln.avg.plot
+
+
 #plot(fit)
 
 residuals = fit$residuals
 
-# Demonstrate constaant variability
-plot(gauge.avg[,'ln.gain'], residuals)
+# Demonstrate constaant variability by plotting residuals
+plot(gauge.avg[,'ln.gain'], residuals, main = "ln(gain) vs. Residuals of ln(gain)", xlab = "ln(gain)",
+     ylab = "Residual")
+abline(0,0)
 
-# Plots to do
-# x vs. y
-# ln.gain vs. residuals
-# qqplot of residuals
-# data with logged gains averaged into groups of 10
+plot(gauge.avg[,1], residuals, main = "Density vs. Residuals of ln(gain)", xlab = "Density",
+     ylab = "Residual")
+abline(0,0)
+
+
+#fit = lm(gauge.avg[,'ln.gain'] ~ gauge.avg[,1], data = gauge.avg)
+ln.fit = lm(gauge[,'ln.gain'] ~ gauge[,1], data = gauge)
+plot(gauge[,3], ln.fit$residuals)
+abline(0,0)
