507913b53e456157f94d21a451bb890fa29c3117
prateekkulkarni4105 <prateekcertain@gmail.com>
2016-03-03 20:34:43 -0800

    Final

--- hw4/289-HW4-Prateek.R
+++ hw4/289-HW4-Prateek.R
@@ -10,14 +10,21 @@ abline(lm2$coefficients[1], lm2$coefficients[2])
 new.gauge = data.frame(log.gain=c(log(38.6),log(426.7)))
 predict(lm2, newdata = new.gauge)
 
-avg.gain = tapply(gauge$log.gain, as.factor(gauge$density), FUN = mean)
-avg.gauge = data.frame(density=as.numeric(names(avg.gain)), log.gain=avg.gain)
+avg.log.gain = tapply(gauge$log.gain, as.factor(gauge$density), FUN = mean)
+avg.gain = tapply(gauge$gain, as.factor(gauge$density), FUN = mean)
+avg.gauge = data.frame(density=as.numeric(names(avg.gain)), log.gain=avg.log.gain, gain=avg.gain)
 plot(y=avg.gauge$density, x=avg.gauge$log.gain)
 abline(lm2.avg$coefficients[1], lm2.avg$coefficients[2])
 
+#Log of gain
 lm2.avg = lm(density~log.gain, data=avg.gauge)
 new.gain = data.frame(log.gain=c(log(38.6),log(426.7)))
 predict(lm2.avg, newdata = new.gain)
+
+#Gain
+lm2.avg.2 = lm(density~gain, data=avg.gauge)
+new.gain.2 = data.frame(gain=c(38.6,426.7))
+predict(lm2.avg.2, newdata = new.gain.2)
 
 # EXTRA
 lm3.avg = lm(density~log.gain, data=avg.gauge)
@@ -25,7 +32,7 @@ new.density = data.frame(density=c(log(38.6),log(426.7)))
 predict(lm3.avg, newdata = new.density)
 
 # PUT BANDS AROUND IT
-plot_conf_band = function(x, y, p){
+plot_conf_band = function(x, y, p, title, xlabel, ylabel){
   # Create Vandermonde matrix
   X = matrix(1, nrow=length(x), ncol=1)
   for(i in 1:p){
@@ -50,7 +57,9 @@ plot_conf_band = function(x, y, p){
   library(ggplot2)
   ggplot() + geom_line(aes(y=upper, x=x)) + geom_line(aes(y=lower, x=x), col="blue") + 
     geom_line(aes(y=ycap, x=x), col="red") +
+    ggtitle(label = title) + xlab(xlabel) + ylab(ylabel)+
+    geom_point(aes(x=x, y=y), col="red")+
     geom_polygon(aes(x=c(temp_tablesort$x,rev(temp_tablesort$x)),y=c(temp_tablesort$lower,rev(temp_tablesort$upper))), col="green", fill="green", alpha=0.5)
 }
 
-plot_conf_band()
+plot_conf_band(gauge$gain, gauge$density, 1, title = "Density vs Log of Gain", xlabel = "Log of Gain", ylabel = "Density")
