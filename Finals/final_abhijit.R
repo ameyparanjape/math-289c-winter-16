887576d0ae06f5f8e8fb6c09e9096ec078054f60
dexter11235813 <abgairola@gmail.com>
2016-03-10 01:30:09 -0800

    Parallelized the Random Forest. takes ~10 mins to run on the entire dataset

--- Finals/final_abhijit.R
+++ Finals/final_abhijit.R
@@ -1,7 +1,11 @@
-setwd("C:/Users/abhijit331/Desktop/289 Finals")
+#setwd("C:/Users/abhijit331/Desktop/289 Finals")
+setwd("/home/abhijit331/Desktop/Math 289")
 #install.packages("zoo")
+#install.packages("doParallel")
+library(doParallel)
 library(zoo)
 library(randomForest)
+library(e1071)
 ####
 fillNA <- function(S) 
 {
@@ -25,9 +29,26 @@ train_train = train[1:100000,]
 labels = train[100001:dim(train)[1],1]
 train_test = train[100001:dim(train)[1],-1]
 
-rf = randomForest(as.factor(target) ~ .,data = train_train )
-pred = predict(rf,train_test)
+ntree = 100; numCore = 4
+rep <- ntree/numCore # tree / numCore
+registerDoParallel(cores=numCore)
+begTime <- Sys.time()
+random.forest <- foreach(ntree=rep(rep, numCore), .combine=combine,
+              .packages="randomForest") %dopar%
+  randomForest(as.factor(target) ~ . , data = train_train,ntree = ntree,mtry = 10,importance = T,replace = F)
+#       randomForest(X.sub, y.sub,
+#                   ntree=ntree#                   mtry=10,
+#                   importance=TRUE,
+#                   na.action=na.roughfix, #can also use na.action = na.omit
+#                   replace=FALSE)
+
+
+#rf = randomForest(as.factor(target) ~ .,data = train_train )
+pred = predict(random.forest,train_test)
 length(which(pred == labels))/dim(train_test)[1]
+runTime <- Sys.time()-begTime
+runTime
+varImpPlot(random.forest)
 # 78% correct classification on the Validation set. Data is trained on 100k datapoints, with 114 variables 
 # included in the training set. 
 test = read.csv("test.csv", header = T)
@@ -40,3 +61,21 @@ test = test[,-1]
 test = data.frame(lapply(test,fillNA))
 test$target = -1
 test$target = predict(rf,test)
+
+# SVM 
+ntree = 100; numCore = 4
+rep <- ntree/numCore # tree / numCore
+registerDoParallel(cores=numCore)
+begtimesvm <- Sys.time()
+model.svm =foreach(ntree=rep(rep, numCore), .combine=combine,
+                         .packages="e1071") %dopar%
+  svm(as.factor(target) ~ . , data = train_train)
+runtimesvm = Sys.time() - begtimesvm
+#       randomForest(X.sub, y.sub,
+#                   ntree=ntree#                   mtry=10,
+#                   importance=TRUE,
+#                   na.action=na.roughfix, #can also use na.action = na.omit
+#                   replace=FALSE)
+
+
+
