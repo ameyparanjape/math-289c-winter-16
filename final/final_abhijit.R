8d50172937c42b4b7d17bc4f22482f26e9693e81
dexter11235813 <abgairola@gmail.com>
2016-03-28 23:06:57 -0700

    final push

--- final/final_abhijit.R
+++ final/final_abhijit.R
@@ -1,10 +1,12 @@
-#setwd("C:/Users/abhijit331/Desktop/289 Finals")
+setwd("C:/Users/abhijit331/Desktop/289 Finals")
 load("~/Desktop/Math 289/.RData")
-setwd("/home/abhijit331/Desktop/Math 289")
+#setwd("/home/abhijit331/Desktop/Math 289")
 #install.packages("zoo")
 #install.packages("doParallel")
 #install.packages("xgboost")
-library(doParallel)
+install.packages("Matrix")
+#library(doParallel)
+library(Matrix)
 library(zoo)
 library(xgboost)
 library(MASS)
@@ -16,7 +18,10 @@ replaceNA = function(s)
 {
   ifelse(is.na(s),-999,s)
 }
-fillNA <- function(S) 
+train = read.csv("train.csv", header = TRUE)
+train = train[complete.cases(train),]
+train1 = train
+fillNA <- function(S)
 {
   L <- !is.na(S)
   c(S[L][1], S[L])[cumsum(L)+1]
@@ -30,14 +35,13 @@ for(i in 1:dim(train1)[1])
   
 }
 ###
-train = read.csv("train.csv", header = TRUE)
-train1 = train
+
 fac = which(lapply(train,class)=="factor")
 factor = train[,fac]
 factor = factor[,-c(2,8,19)]
 train = train[,-fac]
 train = train[,-1]
-train = data.frame(lapply(train,replaceNA))
+train = data.frame(lapply(train,fillNA))
 # add back the factor terms
 train = cbind(train,factor)
 train = cbind(train,counts)
@@ -95,45 +99,63 @@ z = c()
 cl = makeCluster(4)
 registerDoParallel(cl)
 getDoParWorkers()
-dummy = train_train[1:10000,]
+dummy = train[1:20000,]
 begin = Sys.time()
-model.svm = parallelSVM(as.factor(target) ~ .,data =dummy,numberCores = detectCores(),probability = T)
+model.svm = parallelSVM(as.factor(target) ~ .,data =dummy,numberCores = detectCores())
 end = Sys.time() - begin
 end
 stopCluster(cl)
 registerDoSEQ()
 z =c(z,end*60)
 
-train_test = train_train[100001:dim(train)[1],]
-lab = train_train[100001:dim(train)[1],1]
+tuned = tune(svm,as.factor(target) ~ . , data = dummy,ranges = list(epsilon = seq(0,.3,0.01),cost = 2(0:9)))
+train_test = train[50001:60000,-1]
+lab = train[50001:60000,1]
 predict.svm = predict(model.svm,train_test)
-length(which(pred.svm == labels))/dim(train_test)[1]
-
-
-# Parallel Prediction SVM
-cl = makeCluster(4)
-registerDoParallel(cl)
-split = sort(rank(1:nrow(train_test))%%4)
-svm.prediction = foreach(i = unique(split),.combine = combine,.packages = c("e1071")) %dopar%
-  {as.numeric(predict(model.svm,newdata=train_test[split==i,]))}
-stopCluster(cl)
-registerDoSEQ()
-length(which(svm.prediction == labels))/dim(train_test)[1]
+length(which(predict.svm == lab))/10000
 
 
 
  
 # Extreme Gradient Boosting
-dum = train[1:100000,-1]
-la = train[1:100000,1]
-dumtest = train[100001:110000,-1]
-spar2 = sparse.model.matrix(~.,data = dumtest)
-latest = train[100001:110000,1]
+train = read.csv("train.csv", header = TRUE)
+train = data.frame(lapply(train,replaceNA))
+train = train[,-1]
+dum = train[1:10000,-1]
+la = train[1:10000,1]
+ dumtest = train[10001:20000,-1]
+ spar2 = sparse.model.matrix(~.,data = dumtest)
+latest = train[10001:20000,1]
 spar = sparse.model.matrix(~.,data = dum)
 xgb.model = xgboost(data = spar,label =as.factor(la),max_depth = 9,eta = 1 , nthread = 4,nround = 10,onjective = "binary:logistic")
-impo =xgb.importance(feature_names = spar@Dimnames[[2]],model = xgb.model)
+important =xgb.importance(feature_names = spar@Dimnames[[2]],model = xgb.model)
 testp = predict(xgb.model,newdata = spar2)
 testp=ifelse(testp > 0.5,1,0)
-length(which(testp == latest))
+correct = length(which(testp == latest))
+length(which((testp == latest) & latest == 1))/correct
+length(which((testp == latest) & latest == 0))/correct
 head(testp)
 head(latest)
+
+
+# Extracting the top 20 features from the XGBoost
+features = impo[1:20]$Feature
+train = read.csv("train.csv", header = TRUE)
+train = data.frame(lapply(train,replaceNA))
+reduced.dataset = train[,c("target",features)]
+head(reduced.dataset)
+dim(reduced.dataset)
+
+features.dum = reduced.dataset[1:10000,-1]
+features.la = reduced.dataset[1:10000,1]
+features.dumtest = reduced.dataset[10001:20000,-1]
+features.spar2 = sparse.model.matrix(~.,data = features.dumtest)
+features.latest = reduced.dataset[10001:20000,1]
+features.spar = sparse.model.matrix(~.,data = features.dum)
+features.xgb.model = xgboost(data = features.spar,label =as.factor(features.la),max_depth = 9,eta = 1 , nthread = 4,nround = 10,onjective = "binary:logistic")
+features.impo =xgb.importance(feature_names = features.spar@Dimnames[[2]],model = features.xgb.model)
+features.testp = predict(features.xgb.model,newdata = features.spar2)
+features.testp=ifelse(features.testp > 0.5,1,0)
+features.correct = length(which(features.testp == features.latest))
+length(which((features.testp == features.latest) & features.latest == 1))/features.correct
+length(which((features.testp == features.latest) & features.latest == 0))/features.correct
